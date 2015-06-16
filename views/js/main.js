ee842d61e65fa4796d95762765adb50b53ebca65
Kevin Duell <kdubzot9@gmail.com>
2015-06-16 06:38:08 -0700

    Compute and save array len outside of loop and prefer  getElementByClassName().

--- views/js/main.js
+++ views/js/main.js
@@ -460,7 +460,8 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    var pizzaContainerLen = document.getElementsByClassName("randomPizzaContainer").length;
+    for (var i = 0; i < pizzaContainerLen; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
